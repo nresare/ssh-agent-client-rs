bb70d8210f47d71b09d43c6a6c67f9d9e0c29001
Noa Resare <noa@resare.com>
2023-04-16 22:01:12 +0100

    use String.as_str() instead of &String[..]

--- examples/list.rs
+++ examples/list.rs
@@ -4,7 +4,7 @@ use ssh_agent_client_rs::{Client, Result};
 
 fn main() -> Result<()> {
     let path = env::var("SSH_AUTH_SOCK").expect("Missing env variable");
-    let mut client = Client::connect(Path::new(&path[..]))?;
+    let mut client = Client::connect(Path::new(path.as_str()))?;
     let result = client.list_identities()?;
     println!("Got {} identities: {}", result.len(), result.get(0).unwrap().comment);
     Ok(())
