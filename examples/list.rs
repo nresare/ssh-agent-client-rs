8d0320ad81101cdfc42523eeb01851c26575da97
Noa Resare <noa@resare.com>
2023-04-17 22:43:55 +0100

    add ssh-key as dep and have examples/list print hash and algo

--- examples/list.rs
+++ examples/list.rs
@@ -1,11 +1,18 @@
 use std::env;
 use std::path::Path;
-use ssh_agent_client_rs::{Client, Result};
+use ssh_key::public::PublicKey;
+use ssh_agent_client_rs::{Client, Identity, Result};
 
 fn main() -> Result<()> {
     let path = env::var("SSH_AUTH_SOCK").expect("Missing env variable");
     let mut client = Client::connect(Path::new(path.as_str()))?;
-    let result = client.list_identities()?;
-    println!("Got {} identities: {}", result.len(), result.get(0).unwrap().comment);
+    for identity in client.list_identities()?.iter() {
+        to_string(&identity);
+    };
     Ok(())
+}
+
+fn to_string(identity: &Identity) {
+    let key = PublicKey::from_bytes(identity.public_key.as_ref()).expect("failed to parse public key");
+    println!("256 {} {} {}", key.fingerprint(Default::default()), identity.comment, key.algorithm().to_string())
 }
